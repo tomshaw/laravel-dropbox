3ebc8f47c78c0185276c2976a1cfc9350df9981e
Tom Shaw <tomshaw21@gmail.com>
2024-04-14 07:40:25 -0500

    Added props to Storage Collection.

--- src/Models/StorageCollection.php
+++ src/Models/StorageCollection.php
@@ -6,7 +6,13 @@ use Illuminate\Support\Collection;
 
 class StorageCollection extends Collection
 {
+    public $id;
+    public $user_id;
     public $access_token;
-
+    public $refresh_token;
     public $expires_in;
+    public $token_type;
+    public $uid;
+    public $account_id;
+    public $scope;
 }
